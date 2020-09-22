d480ab5a7e22f2dc22812595a206709cc4be7711
Халяпин Вадим <vadim.halyapin@yandex.ru>
2020-09-22 22:05:02 +0300

    Добавьте файлы проекта.

---
+++ InsertedIfOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+class Programm
+{
+    static void Main()
+    {
+        string txt;
+        Console.WriteLine("Введите текст");
+        txt = Console.ReadLine();
+        if (txt != "")
+        {
+            Console.WriteLine("Спасибо, что ввели текст!");
+            if (txt.Length > 10)
+            {
+                Console.WriteLine("Ого, как много букв!");
+            }
+            else
+            {
+                Console.WriteLine("Ого, как мало букв!");
+            }
+        }
+        else
+            Console.WriteLine("Жаль, что не ввели текст!");
+    }
+}
